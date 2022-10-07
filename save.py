a08e844beb1fb9ae3b14fcaba9b2af132b9fa857
Soonseok <59505830+Soonseok@users.noreply.github.com>
2022-10-07 21:30:09 +0900

    refresh

--- save.py
+++ save.py
@@ -1,8 +1,14 @@
 import csv
+
+
 def save_to_file(jobs):
-  file = open("jobs.csv", mode="w")
-  writer = csv.writer(file)
-  writer.writerow(["title", "company", "location", "link"])
-  for job in jobs:
-    writer.writerow(list(job.values()))
-  return
+    """
+  파일 저장
+  """
+    file = open("jobs.csv", mode="w")
+    writer = csv.writer(file)
+    writer.writerow(["title", "company", "location", "link"])
+    for job in jobs:
+        writer.writerow(list(job.values()))
+    return
+
